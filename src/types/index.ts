8f70bc671e0b3d43634da8eb574e5b9cc0b206ce
Thibault Capelli <capellithibault@gmail.com>
2022-07-19 15:52:07 +0200

    chore: add new types
    Add IActionDetailPayload and IActionPayload.

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,8 +4,18 @@ export interface IActionDetail {
   max_value: number;
 }
 
+export interface IActionDetailPayload {
+  name: string;
+  max_value: number;
+}
+
 export interface IAction {
   _id: string;
   user: string;
   action_detail: IActionDetail;
+}
+
+export interface IActionPayload {
+  user: string;
+  action_detail: IActionDetail['_id'];
 }
